102d4cbde5fe3e5fcf8864e94654643b2f380586
Phillip <bronzel.phillip+github@gmail.com>
2021-05-21 12:10:04 +0200

    Fix Bugs when multiple panes are in use and directly change the settings on the CodeMirror instance, instead of creating a new one

--- main.ts
+++ main.ts
@@ -1,20 +1,8 @@
-import { EditorPosition, MarkdownView, Plugin, WorkspaceLeaf } from 'obsidian';
+import { MarkdownView, Plugin, WorkspaceLeaf } from 'obsidian';
 
 export default class ToggleVimPlugin extends Plugin {
 
-	async toggleVimMode(leaf: WorkspaceLeaf) {
-		//@ts-ignore
-		this.app.vault.setConfig("vimMode", !this.app.vault.getConfig("vimMode"));
-		await this.app.workspace.duplicateLeaf(leaf);
-		//@ts-ignore
-		const cursorPos: EditorPosition = leaf.view.editor.getCursor();
-		leaf.detach();
-		//@ts-ignore
-		this.app.workspace.activeLeaf.view.editor.setCursor(cursorPos);
-	}
-
 	async onload() {
-
 		this.addCommand({
 			id: 'toggle-vim-mode',
 			name: 'Toggle Vim Mode',
@@ -22,14 +10,22 @@ export default class ToggleVimPlugin extends Plugin {
 				let leaf: WorkspaceLeaf = this.app.workspace.activeLeaf;
 				if (leaf.view instanceof MarkdownView) {
 					if (!checking) {
-						this.toggleVimMode(leaf);
+						//@ts-ignore
+						this.app.vault.setConfig("vimMode", !this.app.vault.getConfig("vimMode"));
+						this.app.workspace.iterateAllLeaves(async (i) => {
+							if (i.view instanceof MarkdownView) {
+								//@ts-ignore
+								i.view.editor.cm.setOption("keyMap", this.app.vault.getConfig("vimMode")?"vim":"default");
+								//@ts-ignore
+								i.view.editor.cm.refresh();
+							}
+						});
 					}
 					return true;
 				}
 				return false;
 			}
 		});
-
 	}
 
 	onunload() {
